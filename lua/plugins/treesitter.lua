87834b161c6c9d9eed0970e3cf72b8f383a88045
alexerby <alexandereriksson740@outlook.com>
2024-08-26 07:37:26 +0200

    treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,50 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+
+    config = function ()
+      require'nvim-treesitter.configs'.setup {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+              if ok and stats and stats.size > max_filesize then
+                  return true
+              end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end
+}
