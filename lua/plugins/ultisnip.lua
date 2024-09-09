bfbbeb5d446aa039e2b9c4769448033ff52d14dd
Alexerby <alexandereriksson740@outlook.com>
2024-09-09 10:10:45 +0200

    os dependent ultisnip bindings removed, now in alacritty

--- lua/plugins/ultisnip.lua
+++ lua/plugins/ultisnip.lua
@@ -5,21 +5,9 @@ return {
         vim.g.UltiSnipsSnippetDirectories = {'~/.config/nvim/lua/snippets'}
 
         vim.g.UltiSnipsExpandTrigger="<tab>"
-
-        -- Conditionally set the jump backward trigger based on OS
-        if vim.fn.has("mac") == 1 then
-          vim.g.UltiSnipsJumpForwardTrigger = "<D-j>"  -- Example for macOS
-          vim.g.UltiSnipsJumpBackwardTrigger = "^L "  -- Example for macOS
-        else
-          vim.g.UltiSnipsJumpForwardTrigger="<C-j>"
-          vim.g.UltiSnipsJumpBackwardTrigger="<C-k>"
-        end
+        vim.g.UltiSnipsJumpForwardTrigger="<C-j>"
+        vim.g.UltiSnipsJumpBackwardTrigger="<C-k>"
     end,
 }
-
-
-
-
-
 
 
