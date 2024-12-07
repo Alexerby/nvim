c3afdd9cc8b1a4f6536c16abbfd4f93837148e91
Alexerby <alexandereriksson740@outlook.com>
2024-12-07 13:10:01 +0100

    transparent background for macOS

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,33 +1,13 @@
--- return {
---   require('lazy').setup({
---     {
---       'catppuccin/nvim',
---       name = 'catppuccin', -- ensure the name is defined for proper referencing
---       config = function()
---         require('catppuccin').setup {
---           transparent_background = true,
---         }
---       end
---     },
---     {
---       'LazyVim/LazyVim',
---       dependencies = { 'catppuccin/nvim' }, -- ensure LazyVim waits for catppuccin to load
---       config = function()
---         vim.cmd.colorscheme('catppuccin') -- use proper lua function for colorscheme
---       end
---     }
---   })
--- }
-
 return {
-{
-  "catppuccin/nvim", name = "catppuccin", priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      transparent_background = true,
-      vim.cmd.colorscheme('catppuccin') -- use proper lua function for colorscheme
-    }
-  end
-}
-
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup({
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme('catppuccin')
+    end
+  }
 }
