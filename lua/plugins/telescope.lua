d01d49e04c36b3e399c31c6ef91f0922d450f8ea
alexerby <mail@alexandereriksson.se>
2025-05-18 01:34:24 +0200

    Add: Telescope LSP Reference

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
         vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fr', require('telescope.builtin').lsp_references, {})
 
         -- Without ignore paths
         vim.keymap.set('n', '<leader>ffh', builtin.find_files, {})
@@ -28,4 +29,3 @@ return {
 
     end
 }
-
