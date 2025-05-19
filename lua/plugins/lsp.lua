d56c41b2e17e7c739309e236d676d62bb4a4bd8b
alexerby <mail@alexandereriksson.se>
2025-05-19 21:51:19 +0200

    LSP inline errors

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,121 +1,160 @@
-
 -- TODO: Add looser settings to pyright
 -- TODO: Remove LuaSnip as ultisnip is already in use?
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-        "quangnguyen30192/cmp-nvim-ultisnips",
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    -- "j-hui/fidget.nvim",
+    "quangnguyen30192/cmp-nvim-ultisnips",
+    "nvimtools/none-ls.nvim",
+  },
+
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp = require('cmp')
+    local cmp_lsp = require("cmp_nvim_lsp")
+
+
+
+
+  vim.diagnostic.config({
+    virtual_text = {
+      prefix = '●', -- Could be '■', '▶', '▎', '●'
+      spacing = 2,
     },
+    signs = true,           -- show signs in gutter
+    underline = true,       -- underline errors/warnings
+    update_in_insert = true, -- update diagnostics while typing
+    severity_sort = true,   -- sort diagnostics by severity
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  })
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities()
+    )
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                -- "rust_analyzer",
-                "pyright",
-                -- "gopls",
-                "texlab",
+    -- Mason setup
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "texlab",
+      },
+
+      handlers = {
+        -- Default handler for all servers except the ones we override below
+        function(server_name)
+          if server_name == "tsserver" then
+            server_name = "ts_ls"
+          end
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+
+        -- Pyright setup explicitly
+        ["pyright"] = function()
+          lspconfig.pyright.setup({
+            capabilities = capabilities,
+            settings = {
+              python = {
+                analysis = {
+                  typeCheckingMode = "strict", -- or "off", "strict"
+                  autoSearchPaths = true,
+                  useLibraryCodeForTypes = true,
+                },
+              },
             },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                          -- https://github.com/neovim/nvim-lspconfig/pull/3232
-                          -- https://github.com/neovim/nvim-lspconfig/pull/3232#issuecomment-2331025714 || Temporary fix from this github comment
-                          if server_name == "tsserver" then
-                            server_name = "ts_ls"
-                          end
-                          local capabilities = require("cmp_nvim_lsp").default_capabilities()
-                          require("lspconfig")[server_name].setup({
-                            capabilities = capabilities,
-                          })
-                        end,
+          })
+        end,
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
-
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
+        -- Zig language server
+        zls = function()
+          lspconfig.zls.setup({
+            root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+            settings = {
+              zls = {
+                enable_inlay_hints = true,
+                enable_snippets = true,
+                warn_style = true,
+              },
             },
-            mapping = cmp.mapping.preset.insert({
-                ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-                { name = 'ultisnips' },
-            }, {
-                { name = 'buffer' },
-            })
-        })
+          })
+          vim.g.zig_fmt_parse_errors = 0
+          vim.g.zig_fmt_autosave = 0
+        end,
 
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
+        -- Lua language server
+        ["lua_ls"] = function()
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = { version = "Lua 5.1" },
+                diagnostics = {
+                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                },
+              },
             },
-        })
-    end
+          })
+        end,
+      },
+    })
+
+    -- CMP setup
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources(
+        {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'ultisnips' },
+        },
+        {
+          { name = 'buffer' },
+        }
+      )
+    })
+
+    -- Diagnostics UI setup
+    vim.diagnostic.config({
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+  end
 }
-
