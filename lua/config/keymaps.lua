11e5d18e533f07f696597ad4df9dbedc86d29cf6
alexerby <mail@alexandereriksson.se>
2025-05-22 00:52:18 +0200

    Remove remap script and move leaders to keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,13 @@
--- TODO: Move the keybindings into pluggings as possible
-
 -- Keymap Helper
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -----------------------------------------------------------
--- General / Misc
+-- General
 -----------------------------------------------------------
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 map('i', 'jj', '<ESC>', opts)
 map('n', '<leader>d', '<Cmd>nohlsearch<CR>', opts)
 map('n', '<F4>', ':w<CR>:!python3 %<CR>', opts)
--- lua/config/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.g.BASH_Ctrl_j = 'off'
